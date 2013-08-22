42699982a47c36a19604eadff256f87038aa33d2
Raleigh Fatoki <fatoki09@yahoo.com>
2013-08-21 23:36:25 -0500

    Responsive, working, changed logo and deleted products

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mystore::Application.config.secret_token = '5f93a54b90df6b4ad47eaee6c24bb7b7384e42564c79e294c2f05638ac7474c1e3956c5efd076b8537762e5ae6ddeee0a9cca7615eb8c9bbb66a8fa7d529b713'
